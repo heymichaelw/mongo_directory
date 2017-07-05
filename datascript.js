b4bb02f33e322a3057ac2c265629fa7a69d8cd7d
Michael <heymichaelw@gmail.com>
2017-07-05 17:01:30 -0400

    recreated users collection

--- datascript.js
+++ datascript.js
@@ -3,9 +3,9 @@ const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect("mongodb://localhost:27017/mwrobotdb", function(error, db){
   const collection = db.collection("users");
-  // data.users.forEach(function(user){
-  //   collection.insert({id: user.id, username: user.username, name: user.name, avatar: user.avatar, email: user.email, university: user.university, job: user.job, company: user.company, skills: user.skills, phone: user.phone});
-  // });
+   data.users.forEach(function(user){
+     collection.insert(user);
+   });
   // data.users.forEach(function(user){
   //   collection.update({}, {id: user.id, username: user.username, name: user.name, avatar: user.avatar, email: user.email, university: user.university, job: user.job, company: user.company, skills: user.skills, phone: user.phone, address: user.address});
   // });
